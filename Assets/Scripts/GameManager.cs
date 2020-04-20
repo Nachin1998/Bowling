491dd28f8df806fb2d16516683e56c4431a09ac5
Ignacio <Ignacio.f.l@hotmail.com>
2020-04-20 13:11:29 -0300

    Added ball, pines, and gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+    public List<Pine> pines = new List<Pine>();
+    Rigidbody rb;
+    float power;
+    void Start()
+    {
+        rb = ball.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            power += 100;
+        }
+         
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * power);
+            power = 0;
+        }
+
+        if (pines[0].IsPineDown())
+        {
+            ball.transform.position = new Vector3(0, 1, -19);
+        }        
+    }
+}
---
+++ Assets/Scripts/Pine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsPineDown()
+    {
+        if (gameObject.transform.rotation.x > 50 || gameObject.transform.rotation.x < -50 ||
+           gameObject.transform.rotation.z > 50 || gameObject.transform.rotation.z < -50)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
